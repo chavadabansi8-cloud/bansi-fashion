7c3de0943cbc987ca4c9025d2f894b5f97515611
Bansi Fashion <chavadabansi8@gmil.com>
2026-08-25 22:54:11 +0530

    Fix mobile header layout wrapping and native status bar insets

--- android/app/src/main/java/com/bansifashion/app/MainActivity.java
+++ android/app/src/main/java/com/bansifashion/app/MainActivity.java
@@ -1,5 +1,19 @@
 package com.bansifashion.app;
 
+import android.os.Bundle;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content), (view, insets) -> {
+            int statusBarHeight = insets.getInsets(WindowInsetsCompat.Type.statusBars()).top;
+            view.setPadding(0, statusBarHeight, 0, 0);
+            return insets;
+        });
+    }
+}
